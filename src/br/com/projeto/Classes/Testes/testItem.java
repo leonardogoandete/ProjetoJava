00443157c9ede75d8bae92b6f739fe176d0245d4
Leonardo Goandete <leonardo.goandete@hotmail.com>
2021-11-19 13:30:50 -0300

    Ajustes

--- src/br/com/projeto/Classes/Testes/testItem.java
+++ src/br/com/projeto/Classes/Testes/testItem.java
@@ -15,7 +15,7 @@ public class testItem {
         adiciona_itens.add(new Item("Caneta","Caneta Azul","insumo",50,2.12f));
         adiciona_itens.add(new Item("Caneta","Caneta Vermelha","insumo",25,2.55f));
         System.out.println("Setor autorizado a solicitar mouse:");
-        for (int i=0; i <= adiciona_itens.size(); i++) {
+        for (int i=0; i < adiciona_itens.size(); i++) {
             System.out.println(adiciona_itens.get(i));
         }
 
