93dc10fa0f4c5752f906997337dd4151c8bdba78
leogoandete <leonardo.goandete@hotmail.com>
2021-11-24 13:23:25 -0300

    Implementado relacionamento setor.

--- src/br/com/projeto/Classes/Testes/testUsuarioSetor.java
+++ src/br/com/projeto/Classes/Testes/testUsuarioSetor.java
@@ -1,29 +1,24 @@
 package br.com.projeto.Classes.Testes;
 
 import br.com.projeto.Classes.Setor;
-import br.com.projeto.Classes.Solicitacao;
 import br.com.projeto.Classes.Usuarios.Usuario;
 
-import java.util.ArrayList;
 
 public class testUsuarioSetor {
     public static void main(String[] args) {
         Setor rh = new Setor("Recursos Humanos");
         Setor ti = new Setor("Tecnologia");
 
-        Usuario leonardo = new Usuario("leo_goandete","12345",rh);
+        Usuario leonardo = new Usuario("leo_goandete","12345",ti);
 
         leonardo.verificaLogin("leo_goandete","12345");
         System.out.println("Login:"+leonardo.getLogin());
-        System.out.println("Saldo:"+leonardo.getSaldoCota());
+        System.out.println("Saldo restante:"+leonardo.getSaldoCota());
         System.out.println("Perfil:"+leonardo.getTipo());
         System.out.println("Setor:"+leonardo.getSetor().getNome());
 
-        System.out.println(leonardo.setor.itensLiberados(leonardo.getSetor().getNome()));
-
-
-
-
+        System.out.println("Itens liberados:\n"+leonardo.setor.itensLiberados(leonardo.setor));
+        System.out.println("Cota liberada setor:"+leonardo.setor.cotaLiberadaSetor());
 
 
     }
